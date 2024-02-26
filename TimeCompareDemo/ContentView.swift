6b8473e15acc4622947bf7fc0de07d8f35df57e7
Jack <tcharge_developer-04@terramotors.co.jp>
2024-02-26 14:19:48 +0900

    Initial Commit

---
+++ TimeCompareDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimeCompareDemo
+//
+//  Created by Chun Chieh Chang on 2024/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimeCompareDemo/TimeCompareDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeCompareDemoApp.swift
+//  TimeCompareDemo
+//
+//  Created by Chun Chieh Chang on 2024/02/26.
+//
+
+import SwiftUI
+
+@main
+struct TimeCompareDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
